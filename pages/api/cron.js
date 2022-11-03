f5721b7bf8e78bc3ab342b8fc91fee1f5dd79e9f
geckoxys <edward.andre.23@gmail.com>
2022-11-02 22:14:56 -0400

    cron

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -1,4 +1,12 @@
+import NextCors from "nextjs-cors";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 require('dotenv').config()
+
+async function deleteAll(log) {
+    await prisma.log.deleteMany();
+}
 
 export default async function handler(req, res) {
 
@@ -7,13 +15,18 @@ export default async function handler(req, res) {
         try {
             const { authorization } = req.headers;
 
-            // console.log("funciono")
 
             if (authorization === `Bearer ${process.env.API_SECRET_KEY}`) {
-            // console.log("funciono2")
-            res.status(200).json({ success: true });
+                await deleteAll()
+                    .catch((e) => {
+                        return res.status(500).send("error eliminando");
+                    })
+                    .finally(async () => {
+                        await prisma.$disconnect();
+                    });
+                res.status(200).json({ success: true });
             } else {
-            res.status(401).json({ success: false });
+                res.status(401).json({ success: false });
             }
         } catch (err) {
             res.status(500).json({ statusCode: 500, message: err.message });
